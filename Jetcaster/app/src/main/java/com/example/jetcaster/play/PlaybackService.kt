729327dc6836a8d70b7edc67f8a4676e17b38366
lhk <fujingjunben@gmail.com>
2022-10-03 20:24:50 +0800

    Update PlaybackService.kt

--- Jetcaster/app/src/main/java/com/example/jetcaster/play/PlaybackService.kt
+++ Jetcaster/app/src/main/java/com/example/jetcaster/play/PlaybackService.kt
@@ -1,19 +1,33 @@
 package com.example.jetcaster.play
 
+import android.content.Intent
+import androidx.media3.common.MediaItem
+import androidx.media3.common.Player
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.session.MediaSession
 import androidx.media3.session.MediaSessionService
 
 class PlaybackService: MediaSessionService() {
+    public val URL = "MEDIA_URL";
     private var mediaSession: MediaSession? = null
+    private val player = ExoPlayer.Builder(this).build()
 
     override fun onCreate() {
         super.onCreate()
-        val player = ExoPlayer.Builder(this).build()
         mediaSession = MediaSession.Builder(this, player).build()
     }
+
     override fun onGetSession(controllerInfo: MediaSession.ControllerInfo): MediaSession? {
         return mediaSession
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        if (intent != null && intent.hasExtra(URL)) {
+            intent.getStringExtra(URL)?.let {
+                player.setMediaItem(MediaItem.fromUri(it))
+            }
+        }
+        return super.onStartCommand(intent, flags, startId)
     }
 
     override fun onDestroy() {
