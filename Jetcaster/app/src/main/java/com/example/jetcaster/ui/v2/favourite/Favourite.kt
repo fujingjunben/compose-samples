c884f1a5ec75fd620c48c873d38a92dbafbb2d0b
haikuan <fujingjunben@gmail.com>
2022-10-10 00:17:38 +0800

    windowInsets

--- Jetcaster/app/src/main/java/com/example/jetcaster/ui/v2/favourite/Favourite.kt
+++ Jetcaster/app/src/main/java/com/example/jetcaster/ui/v2/favourite/Favourite.kt
@@ -1,14 +1,20 @@
 package com.example.jetcaster.ui.v2.favourite
 
-import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
-fun Favourite(){
-    Surface(modifier = Modifier.systemBarsPadding()) {
+fun Favourite() {
+    Surface(
+        modifier = Modifier
+            .systemBarsPadding()
+            .windowInsetsPadding(
+                WindowInsets.systemBars.only(WindowInsetsSides.Horizontal)
+            )
+    ) {
         Text(text = "Favourite")
     }
 }
