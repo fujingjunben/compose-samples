c4248c8de1f576bcf9be481be99547091f4b4900
haikuan <fujingjunben@gmail.com>
2022-11-05 21:52:23 +0800

    delete unused code

--- Jetcaster/app/src/main/java/com/example/jetcaster/ui/home/HomeViewModel.kt
+++ Jetcaster/app/src/main/java/com/example/jetcaster/ui/home/HomeViewModel.kt
@@ -30,46 +30,11 @@ import kotlinx.coroutines.launch
 
 class HomeViewModel(
     private val podcastsRepository: PodcastsRepository = Graph.podcastRepository,
-    private val podcastStore: PodcastStore = Graph.podcastStore
 ) : ViewModel() {
-    // Holds our currently selected home category
-    private val selectedCategory = MutableStateFlow(HomeCategory.Discover)
-    // Holds the currently available home categories
-    private val categories = MutableStateFlow(HomeCategory.values().asList())
-
-    // Holds our view state which the UI collects via [state]
-    private val _state = MutableStateFlow(HomeViewState())
 
     private val refreshing = MutableStateFlow(false)
 
-    val state: StateFlow<HomeViewState>
-        get() = _state
-
     init {
-        viewModelScope.launch {
-            // Combines the latest value from each of the flows, allowing us to generate a
-            // view state instance which only contains the latest values.
-            combine(
-                categories,
-                selectedCategory,
-                podcastStore.followedPodcastsSortedByLastEpisode(limit = 20),
-                refreshing
-            ) { categories, selectedCategory, podcasts, refreshing ->
-                HomeViewState(
-                    homeCategories = categories,
-                    selectedHomeCategory = selectedCategory,
-                    featuredPodcasts = podcasts,
-                    refreshing = refreshing,
-                    errorMessage = null /* TODO */
-                )
-            }.catch { throwable ->
-                // TODO: emit a UI error here. For now we'll just rethrow
-                throw throwable
-            }.collect {
-                _state.value = it
-            }
-        }
-
         refresh(force = false)
     }
 
@@ -88,26 +53,5 @@ class HomeViewModel(
             refreshing.value = false
         }
     }
-
-    fun onHomeCategorySelected(category: HomeCategory) {
-        selectedCategory.value = category
-    }
-
-    fun onPodcastUnfollowed(podcastUri: String) {
-        viewModelScope.launch {
-            podcastStore.unfollowPodcast(podcastUri)
-        }
-    }
 }
 
-enum class HomeCategory {
-    Library, Discover
-}
-
-data class HomeViewState(
-    val featuredPodcasts: List<PodcastWithExtraInfo> = emptyList(),
-    val refreshing: Boolean = false,
-    val selectedHomeCategory: HomeCategory = HomeCategory.Discover,
-    val homeCategories: List<HomeCategory> = emptyList(),
-    val errorMessage: String? = null
-)
